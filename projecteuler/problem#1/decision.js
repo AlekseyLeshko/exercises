5f076778002f3ed2c41e51d2d9c88bd99700651f
AlekseyLeshko <alekseyleshko@gmail.com>
2016-10-05 22:01:32 +0400

    add work decision for problem#1

--- projecteuler/problem#1/decision.js
+++ projecteuler/problem#1/decision.js
@@ -1,7 +1,15 @@
 import test from 'ava';
 
-let getSumMultiples3and5 = function(val) {
-  return 23;
+let getSumMultiples3and5 = function(maxVal) {
+  let sum = 0;
+
+  for (var i = maxVal- 1; i > 0; i--) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
 }
 
 test('sum natural numbers below 10 equals 23', t => {
@@ -12,3 +20,10 @@ test('sum natural numbers below 10 equals 23', t => {
   t.is(actual, expected);
 });
 
+test('answer', t => {
+  const expected = 233168;
+
+  let actual = getSumMultiples3and5(1000);
+
+  t.is(actual, expected);
+});
