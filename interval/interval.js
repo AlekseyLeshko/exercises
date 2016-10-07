83fdc095a6e9365b353e7ccc6356b82047d4fa1b
AlekseyLeshko <alekseyleshko@gmail.com>
2016-10-07 23:55:32 +0400

    update tests for interval

--- interval/interval.js
+++ interval/interval.js
@@ -1,4 +1,4 @@
-import test from 'ava';
+import {describe} from 'ava-spec';
 import myFirstDecesion from './first-decision';
 
 let logArr = [];
@@ -8,37 +8,48 @@ let logger = function(val) {
 
 let interval = myFirstDecesion;
 
-test.beforeEach(t => {
-  logArr = [];
-});
+describe('interval', it => {
+  it.beforeEach(t => {
+    logArr = [];
+  });
 
-test('simple sequence', t => {
-  const expected = [0, 1, 2, 3, 4, 5];
-  interval(0, 5, 1, logger);
-  t.deepEqual(logArr, expected);
-});
+  it('sequence with step the one', t => {
+    const expected = [0, 1, 2, 3, 4, 5];
 
-test('with step equals 3', t => {
-  const expected = [0, 3, 6, 9];
-  interval(0, 10, 3, logger);
-  t.deepEqual(logArr, expected);
-});
+    interval(0, 5, 1, logger);
 
-test('negative step', t => {
-  const expected = [5, 3, 1, -1, -3, -5];
-  interval(5, -5, -2, logger);
-  t.deepEqual(logArr, expected);
-});
+    t.deepEqual(logArr, expected);
+  });
 
-test('empty logger', t => {
-  const expected = [];
-  interval(0, 2, -1, logger);
-  t.deepEqual(logArr, expected);
-});
+  it('sequence with step the 3', t => {
+    const expected = [0, 3, 6, 9];
 
-test('zero step', t => {
-  const expected = [];
-  interval(0, 3, 0, logger);
-  t.deepEqual(logArr, expected);
-});
+    interval(0, 10, 3, logger);
+    
+    t.deepEqual(logArr, expected);
+  });
 
+  it('sequence with negative step the two', t => {
+    const expected = [5, 3, 1, -1, -3, -5];
+    
+    interval(5, -5, -2, logger);
+
+    t.deepEqual(logArr, expected);
+  });
+
+  it('initial value is less than the final', t => {
+    const expected = [];
+
+    interval(0, 2, -1, logger);
+
+    t.deepEqual(logArr, expected);
+  });
+
+  it('sequence with zero step', t => {
+    const expected = [];
+
+    interval(0, 3, 0, logger);
+
+    t.deepEqual(logArr, expected);
+  });
+});
