bb25f02aeadfffe64640a5fa71f184e7dbb9d804
AlekseyLeshko <alekseyleshko@gmail.com>
2016-10-15 21:33:30 +0400

    update base decision for problem#2

--- projecteuler/problem#2/decisions/base-decision.js
+++ projecteuler/problem#2/decisions/base-decision.js
@@ -1,19 +1,28 @@
-let getSumFibonacciFor = function(max) {
+let getSum = function(max) {
   let sum = 0;
+  let isFinish = isLessOrEqual.bind(null, max);
 
-  function fibonacci(prev, curr) {
-    if (curr >= max) {
+  function fib(prev, curr) {
+    if (isFinish(curr)) {
       return sum;
     }
 
-    if (curr % 2 === 0) {
+    if (isEven(curr)) {
       sum += curr;
     }
 
-    return fibonacci(curr, prev + curr);
+    return fib(curr, prev + curr);
   }
 
-  return fibonacci(1, 2);
+  return fib(1, 2);
 }
 
-module.exports = getSumFibonacciFor;
+function isLessOrEqual(max, val) {
+  return max <= val; 
+}
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+module.exports = getSum;
