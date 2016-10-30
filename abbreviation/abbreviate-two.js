f4f1a0174d7d99ac6eed3e774d8bdc2596e2c01b
AlekseyLeshko <alekseyleshko@gmail.com>
2016-10-31 00:12:36 +0400

    add second decision for abbreviation task

---
+++ abbreviation/abbreviate-two.js
@@ -0,0 +1,30 @@
+function getAbbreviate(str) {
+  let res = str[0];
+  res += str.length - 2;
+  res += str[str.length - 1];
+
+  return res;
+}
+
+function isSmallWord(str) {
+  return str.length <= 3;
+}
+
+let abbreviate = function(str) {
+  let arr = str.split(/[\s-!,]+/);
+  arr.map(function(curr, index, add) {
+    if (isSmallWord(curr)) {
+      return;
+    }
+
+    if (str.indexOf(curr) === -1) {
+      return;
+    }
+    const abbreviateStr = getAbbreviate(curr);
+    str = str.replace(curr, abbreviateStr);
+  });
+
+  return str;
+}
+
+export default abbreviate;
