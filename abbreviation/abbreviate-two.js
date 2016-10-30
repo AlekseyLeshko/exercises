e9bb32431338ecffbb0eff2f1b89f6ef2117b93a
AlekseyLeshko <alekseyleshko@gmail.com>
2016-10-31 00:24:52 +0400

    fix

--- abbreviation/abbreviate-two.js
+++ abbreviation/abbreviate-two.js
@@ -1,16 +1,4 @@
-function getAbbreviate(str) {
-  let res = str[0];
-  res += str.length - 2;
-  res += str[str.length - 1];
-
-  return res;
-}
-
-function isSmallWord(str) {
-  return str.length <= 3;
-}
-
-let abbreviate = function(str) {
+export default function(str) {
   let arr = str.split(/[\s-!,]+/);
   arr.map(function(curr, index, add) {
     if (isSmallWord(curr) || !str.includes(curr)) {
@@ -24,4 +12,14 @@ let abbreviate = function(str) {
   return str;
 }
 
-export default abbreviate;
+function getAbbreviate(str) {
+  let res = str[0];
+  res += str.length - 2;
+  res += str[str.length - 1];
+
+  return res;
+}
+
+function isSmallWord(str) {
+  return str.length <= 3;
+}
