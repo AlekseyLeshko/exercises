2198c7b7d9dd471fc9b9596c0a88427d9cb0c2b6
AlekseyLeshko <alekseyleshko@gmail.com>
2016-10-31 13:13:30 +0400

    fix test

--- abbreviation/test.js
+++ abbreviation/test.js
@@ -2,78 +2,34 @@ import {describe} from 'ava-spec';
 import abbreviate from './abbreviate'
 import abbreviateTwo from './abbreviate-two'
 
-describe('abbreviate', it => {
-  it('big sentence', t => {
-    const expected = 'e6t-r3s are r4y fun!';
-    const str = 'elephant-rides are really fun!';
+describe('abbreviate:', it => {
+  describe('custom tests:', it => {
+    let str;
+    let expected;
 
-    let actual = abbreviate(str);
+    it.afterEach(t => {
+      t.is(abbreviate(str), expected);
+      t.is(abbreviateTwo(str), expected);
+    });
 
-    t.is(actual, expected);
-  });
+    it('big sentence', t => {
+      expected = 'e6t-r3s are r4y fun!';
+      str = 'elephant-rides are really fun!';
+    });
 
-  it('internationalization', t => {
-    const expected = 'i18n';
-    const str = 'internationalization';
+    it('internationalization', t => {
+      expected = 'i18n';
+      str = 'internationalization';
+    });
 
-    let actual = abbreviate(str);
+    it('finish', t => {
+      expected = 'You n2d, n2d not w2t, to c6e t2s c2e-w2s m5n';
+      str = 'You need, need not want, to complete this code-wars mission';
+    });
 
-    t.is(actual, expected);
-  });
-
-  it('finish', t => {
-    const expected = 'You n2d, n2d not w2t, to c6e t2s c2e-w2s m5n';
-    const str = 'You need, need not want, to complete this code-wars mission';
-
-    let actual = abbreviate(str);
-
-    t.is(actual, expected);
-  });
-
-  it('on on a', t => {
-    const expected = 'on on a';
-    const str = 'on on a';
-
-    let actual = abbreviate(str);
-
-    t.is(actual, expected);
-  });
-});
-
-describe('abbreviate tow', it => {
-  it('big sentence', t => {
-    const expected = 'e6t-r3s are r4y fun!';
-    const str = 'elephant-rides are really fun!';
-
-    let actual = abbreviateTwo(str);
-
-    t.is(actual, expected);
-  });
-
-  it('internationalization', t => {
-    const expected = 'i18n';
-    const str = 'internationalization';
-
-    let actual = abbreviateTwo(str);
-
-    t.is(actual, expected);
-  });
-
-  it('finish', t => {
-    const expected = 'You n2d, n2d not w2t, to c6e t2s c2e-w2s m5n';
-    const str = 'You need, need not want, to complete this code-wars mission';
-
-    let actual = abbreviateTwo(str);
-
-    t.is(actual, expected);
-  });
-
-  it('on on a', t => {
-    const expected = 'on on a';
-    const str = 'on on a';
-
-    let actual = abbreviateTwo(str);
-
-    t.is(actual, expected);
+    it('on on a', t => {
+      expected = 'on on a';
+      str = 'on on a';
+    });
   });
 });
