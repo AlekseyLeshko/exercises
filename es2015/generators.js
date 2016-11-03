8b0df3c55dd6f9a5085a1c70bdd499f9a623fad6
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-03 23:21:27 +0400

    add fibonacci generator

--- es2015/generators.js
+++ es2015/generators.js
@@ -5,6 +5,17 @@ function* myGenerator() {
   yield 3;
 }
 
+function* fibonacci() {
+  let prev = 0;
+  let curr = 1;
+  while(true) {
+    let newNumber = curr + prev; 
+    prev = curr;
+    curr = newNumber;
+    yield newNumber;
+  }
+}
+
 describe('generators:', it => {
   it('sample generator', t => {
     const gen = myGenerator();
@@ -13,5 +24,16 @@ describe('generators:', it => {
 
     actual = gen.next().value;
     t.is(actual, 3);
+  });
+
+  it('fibonacci generator', t => {
+    const gen = fibonacci();
+
+    t.is(gen.next().value, 1);
+    t.is(gen.next().value, 2);
+    t.is(gen.next().value, 3);
+    t.is(gen.next().value, 5);
+    t.is(gen.next().value, 8);
+    t.is(gen.next().value, 13);
   });
 });
