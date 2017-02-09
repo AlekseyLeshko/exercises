5c1713dbce39ceaa55fa3dbfaa8d18bb17daeddc
AlekseyLeshko <alekseyleshko@gmail.com>
2017-02-09 23:35:34 +0400

    add base decision for toCamelCase

--- codewars/toCamelCase/test.js
+++ codewars/toCamelCase/test.js
@@ -1,11 +1,30 @@
-import {
-  describe
-} from 'ava-spec';
-import toCamelCase from './toCamelCase';
+import { describe } from 'ava-spec';
+import toCamelCase, { isSeparator } from './toCamelCase';
 
 describe('toCamelCase:', it => {
-  it('should', t => {
-    t.truthy(true);
+  it('should convert snake case', t => {
+    const expected = 'helloWorld';
+    const str = 'hello_world';
+
+    const actual = toCamelCase(str);
+
+    t.is(actual, expected);
+  });
+
+  it('should char is separator', t => {
+    const char = '_';
+
+    const actual = isSeparator(char);
+
+    t.truthy(actual);
+  });
+
+  it('should char is not separator', t => {
+    const char = 'a';
+
+    const actual = isSeparator(char);
+
+    t.falsy(actual);
   });
 });
 
--- codewars/toCamelCase/toCamelCase.js
+++ codewars/toCamelCase/toCamelCase.js
@@ -1,2 +1,30 @@
 export default function(string) {
+  let newStr = '';
+
+  string
+    .split('')
+    .reduce((inWord, char) => {
+      if (isSeparator(char)) {
+        return true;
+      }
+
+      if (inWord) {
+        newStr += char.toUpperCase();
+
+        return false;
+      }
+
+      newStr += char;
+
+      return false;
+    }, false);
+
+  return newStr;
 }
+
+export function isSeparator(char) {
+  const separators = '_';
+
+  return separators.includes(char);
+}
+
