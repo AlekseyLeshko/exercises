b057bac5949a12ecc387b6ef04a583be658cf12a
AlekseyLeshko <alekseyleshko@gmail.com>
2017-02-12 16:46:02 +0400

    update

--- codewars/toCamelCase/toCamelCase.js
+++ codewars/toCamelCase/toCamelCase.js
@@ -1,14 +1,12 @@
+import capitalize from '../../tasks/capitalize/capitalize-simple-decision';
+
 export default function(string) {
   const separators = /[_-\s]/ig;
 
   const newStr = string
     .split(separators)
     .filter((word) => word)
-    .map((word, index) => {
-      word = word.toLowerCase();
-
-      return index? word.charAt(0).toUpperCase() + word.substring(1): word;
-    })
+    .map((word, index) => index? capitalize(word): word.toLowerCase())
     .join('');
 
   return newStr;
