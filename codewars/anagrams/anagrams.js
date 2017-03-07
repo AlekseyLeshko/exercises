ebb328981b048c889f731c42c06cbd331a8481ce
AlekseyLeshko <alekseyleshko@gmail.com>
2017-03-08 00:27:35 +0400

    add anagrams task

---
+++ codewars/anagrams/anagrams.js
@@ -0,0 +1,4 @@
+export default function(word, words) {
+  return true;
+}
+
---
+++ codewars/anagrams/test.js
@@ -0,0 +1,9 @@
+import { describe } from 'ava-spec';
+import anagrams from './anagrams'
+
+describe('anagrams:', it => {
+  it('should return all anagrams for word', t => {
+    t.truthy(true);
+  });
+});
+
