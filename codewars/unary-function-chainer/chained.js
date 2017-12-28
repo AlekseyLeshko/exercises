cb41af2bee3e416de0fe0ed87723c2e70579db30
AlekseyLeshko <alekseyleshko@gmail.com>
2017-12-29 01:57:04 +0400

    Add first solution for kata

--- codewars/unary-function-chainer/chained.js
+++ codewars/unary-function-chainer/chained.js
@@ -1,4 +1,9 @@
-export default () => {
-  return () => {};
+export default (functions) => {
+  return (val) => {
+    let newVal = functions[0](val);
+    newVal = functions[1](newVal);
+
+    return functions[2](newVal);
+  };
 }
 
