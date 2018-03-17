9eaf078ffa5ea2d5e1b904a7cf65d3717c5bc908
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-18 01:23:38 +0400

    Update decision

--- codewars/enough-is-enough/decision.js
+++ codewars/enough-is-enough/decision.js
@@ -1,19 +1,11 @@
 export const deleteNth = (list, number) => {
   const dictionary = {};
-  const result = [];
 
-  list.forEach(item => {
+  return list.filter(item => {
     if (dictionary[item] === undefined) {
       dictionary[item] = 0;
     }
 
-    if (dictionary[item] + 1 > number) {
-      return;
-    } else {
-      dictionary[item]++;
-      result.push(item);
-    }
+    return dictionary[item]++ < number;
   });
-
-  return result;
 };
