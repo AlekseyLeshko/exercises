c7c1760048f5e2e4576e633dd0556018fbaacfc2
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-18 02:19:49 +0400

    Rework arrow func

--- codewars/enough-is-enough/decision.js
+++ codewars/enough-is-enough/decision.js
@@ -1,7 +1,5 @@
-export const deleteNth = (list, number) => {
-  return list.reduce((arr, item) => {
-    return count(arr, item) < number ? arr.concat(item) : arr;
-  }, []);
-};
+export const deleteNth = (list, number) =>
+  list.reduce((arr, item) =>
+    count(arr, item) < number ? arr.concat(item) : arr, []);
 
 const count = (arr, item) => arr.filter(i => i === item).length;
