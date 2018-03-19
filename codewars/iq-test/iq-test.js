cf0bf1c63ee5978265813e5217c734127d343901
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-20 01:23:30 +0400

    Update decision

--- codewars/iq-test/iq-test.js
+++ codewars/iq-test/iq-test.js
@@ -1,12 +1,8 @@
 export default (numbers) => {
   const list = numbers.split(' ');
-  let number;
-  if (list.filter(num => num % 2 === 0).length === 1) {
-    number = list.find(num => num % 2 === 0);
-  }
-  if (list.filter(num => num % 2 === 1).length === 1) {
-    number = list.find(num => num % 2 === 1);
-  }
+  const number = list.filter(num => num % 2 === 0).length === 1 ?
+    list.find(num => num % 2 === 0) :
+    list.find(num => num % 2 === 1);
 
   return list.indexOf(number) + 1;
 };
