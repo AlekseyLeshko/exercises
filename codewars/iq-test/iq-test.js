066984d7e7d74be42973e9492f3cb24157b02f9a
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-20 01:17:42 +0400

    Add base decision for iq test

--- codewars/iq-test/iq-test.js
+++ codewars/iq-test/iq-test.js
@@ -1,3 +1,12 @@
 export default (numbers) => {
-  return 1;
+  const list = numbers.split(' ');
+  let number;
+  if (list.filter(num => num % 2 === 0).length === 1) {
+    number = list.find(num => num % 2 === 0);
+  }
+  if (list.filter(num => num % 2 === 1).length === 1) {
+    number = list.find(num => num % 2 === 1);
+  }
+
+  return list.indexOf(number) + 1;
 };
