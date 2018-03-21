52a83cfbc275dc2607705e2cf586418c4fc9d4b3
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-22 00:12:18 +0400

    Add first decision

--- codewars/multiplication-tables/multiplication-table.js
+++ codewars/multiplication-tables/multiplication-table.js
@@ -1,3 +1,12 @@
 export default (row, col) => {
-  return [];
+  const res = [];
+  for (let i = 1; i <= row; i++) {
+    const r = [];
+    for (let j = 1; j <= col; j++) {
+      r.push(j * i);
+    }
+    res.push(r);
+  }
+
+  return res;
 };
