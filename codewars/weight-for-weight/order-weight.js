6322b0a4fd4f265f0bb44628547838da5c9cc737
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-24 14:17:17 +0400

    Add decision for weight-for-weight

--- codewars/weight-for-weight/order-weight.js
+++ codewars/weight-for-weight/order-weight.js
@@ -1,2 +1,17 @@
-export default (string) => {
-};
+export default (string) =>
+  string
+    .split(' ')
+    .map((num, index) => ({
+      index, 
+      val: num,
+      weight: num
+        .split('')
+        .reduce((weight, char) =>
+          weight += +char, 0),
+    }))
+    .sort((a ,b) =>
+      a.weight === b.weight ?
+        a.val > b.val :
+        a.weight > b.weight)
+    .map(item => item.val)
+    .join(' ');
